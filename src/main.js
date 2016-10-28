ca28616a54b1e0961e257fe7b55fdff826d7a2b7
Yuri_Yadvizhuk <r1991vm@gmail.com>
2016-10-29 01:34:55 +0300

    animate

---
+++ src/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  skrollr.init({
+    smoothScrolling: true
+  });
+});
+// $(document).ready(function() {
+// // build the tween object
+// var tween = $('.lauer0').KUTE('fromTo',
+
+//  { top: 200 }, { top: 0 }, 
+// { left: 200 }, { left: 0 }, 
+//    {repeat: 1,  easing: 'easingCubicOut'} );
+
+// // start animating
+// $(tween).KUTE('start');
+
+// });
+
